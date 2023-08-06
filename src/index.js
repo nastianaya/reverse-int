0fc159996190841391ac6b3157c1845a54c18455
Your Name <you@example.com>
2023-08-06 21:59:44 +0300

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+const str = n.toString()
+const spl = str.split('')
+const r = spl.reverse()
+const res = r.join('')
+const result = parseFloat(res)
+
+return (result)
 }
